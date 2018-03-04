881d00d1b8b7e068b2bfbea65ca75696c998f8e3
satoukunihiko <satoukunihiko@192.168.0.13>
2018-03-04 23:18:28 +0900

    change logic

--- pykka-python/AkkaMain.py
+++ pykka-python/AkkaMain.py
@@ -4,22 +4,71 @@
 import pykka
 
 MESSAGE = {
-    'ACTOR1_REQUEST' : 'MessageActor1_Request',
-    'ACTOR1_RESPONSE' : 'MessageActor2_Response',
+    'ACTOR1_REQUEST'  : 'MessageActor1_Request',
+    'ACTOR1_RESPONSE' : 'MessageActor1_Response',
+    'ACTOR2_REQUEST'  : 'MessageActor2_Request',
+    'ACTOR2_RESPONSE' : 'MessageActor2_Response',
+    'ACTOR3_REQUEST'  : 'MessageActor3_Request',
+    'ACTOR3_RESPONSE' : 'MessageActor3_Response',
 }
 
-class MyActor(pykka.ThreadingActor):
+class MyActor1(pykka.ThreadingActor):
+    """ Actor1 """
 
     def on_receive(self, request):
         message = request['message']
-        print('Hello, {0}!'.format(message))
+        print('Receive {0}!'.format(message))
+
+
+class MyActor2(pykka.ThreadingActor):
+    """ Actor2 """
+
+    def on_receive(self, request):
+        message = request['message']
+        print('Receive {0}!'.format(message))
+
+
+class MyActor3(pykka.ThreadingActor):
+    """ Actor3 """
+
+    def on_receive(self, request):
+        message = request['message']
+        print('Receive {0}!'.format(message))
+        return {
+            'message' : MESSAGE['ACTOR3_RESPONSE']
+        }
+
 
 def main():
-    actor_ref = MyActor.start()
-    actor_ref.tell({
-        'message' : MESSAGE['ACTOR1_REQUEST']
+
+    ## Send To Actor1
+    actor_ref1 = MyActor1.start()
+    actor_ref2 = MyActor2.start()
+    actor_ref3 = MyActor3.start()
+
+    for ii in range(5):
+        actor_ref1.tell({
+            'message' : MESSAGE['ACTOR1_REQUEST']
+        })
+
+    actor_ref2.tell({
+        'message' : MESSAGE['ACTOR2_REQUEST']
     })
-    actor_ref.stop()
+
+    res_future = actor_ref3.ask(
+        {
+            'message' : MESSAGE['ACTOR2_REQUEST']
+        },
+        block=False
+    )
+
+    response = res_future.get(timeout=1)
+    print('Receive {0}!'.format(response['message']))
+
+    actor_ref1.stop()
+    actor_ref2.stop()
+    actor_ref3.stop()
+
 
 if __name__ == '__main__':
     main()
